9adecc3a027475946ae005085e566c9f36336746
hbeat <heartbeatpkmn@gmail.com>
2021-11-10 17:29:26 +0700

    add some category

--- backend/serializers.py
+++ backend/serializers.py
@@ -1,12 +1,16 @@
 from django.db.models import fields
 from rest_framework import serializers
-from .models import Quiz
+from .models import Category, Quiz
 from django.contrib.auth import get_user_model
 class QuizSerializer(serializers.ModelSerializer):
     class Meta:
         model = Quiz
-        fields = ('id','title','picture','category','creator')
+        fields = ('id','title','picture','category','creator',)
 
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ('name','image','detail',)
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
